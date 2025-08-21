09882c861b8d4d01933ea1580677c2e73a757787
shekhrbhardwaj <shekhrbhardwaj@gmail.com>
2025-08-21 09:48:12 +0530

    Update import for wrapper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import UserProtectedWrapper from './wrapper/userProtectedWrapper';
-import PublicWrapper from './wrapper/PublicWrapper';
+import PublicWrapper from './wrapper/publicWrapper';
 
 
 export const App = () => {
